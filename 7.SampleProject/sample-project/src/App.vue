faf688a1599a2830a9b8259fa07d34255959f2c6
solaris83 <novalis83@gmail.com>
2026-03-16 21:51:29 +0100

    components communication

--- 7.SampleProject/sample-project/src/App.vue
+++ 7.SampleProject/sample-project/src/App.vue
@@ -2,12 +2,43 @@
 import HelloWorld from './components/HelloWorld.vue'
 // Local import
 import ButtonCounter from './components/ButtonCounter.vue';
+import { reactive, ref } from 'vue';
+
+const email = ref('pippo@gmail.com')
+let variableString = ref('')
+
+const contacts = reactive([{
+  "email" : "pippo@gmail.com", 
+  "name" : "pippo", 
+  "phone" : 12223,
+  "variable" : variableString, 
+  "isFavorite" : false
+},
+{
+  "email" : "pluto@gmail.com", 
+  "name" : "pluto", 
+  "phone" : 3333,
+  "variable" : variableString, 
+  "isFavorite" : true
+},
+{
+  "email" : "paperino@gmail.com", 
+  "name" : "paperino", 
+  "phone" : 4444,
+  "variable" : variableString, 
+  "isFavorite" : true
+}])
 
 </script>
 
 <template>
  <!--<HelloWorld />-->
-  <my-component/>
+  <my-component :email="email" :phone="234444" name="fdffdfdffff121111"/>  <!-- for not string (e.g. Number ) we have to use v-bind -->
+  <hr>
+  <p>Variable string: </p><input type="text" placeholder="Enter a value" v-model="variableString" />
+  <div v-for="contact in contacts" :key="contact.name">
+    <my-component :email="contact.email":phone="contact.phone" :name="contact.name" :variable="variableString" :isFavorite="contact.isFavorite"/>
+  </div>
   <ButtonCounter/>
   <ButtonCounter/>
 </template>
--- 7.SampleProject/sample-project/src/components/Component.vue
+++ 7.SampleProject/sample-project/src/components/Component.vue
@@ -1,12 +1,28 @@
 <template>
-    <div>
-        {{ message }}
+    <div style="border: red 1px solid">
+       Name:  {{ name }} <br/>
+       Email:  {{ email }} <br/>
+       Phone: {{ phone }}<br/>
+       Variable string: {{ variable }}<br/>
+       <button @click="handleClick(name)" :class="isFavorite ? 'btn btn-success' : 'btn btn-warning'">
+        {{ isFavorite ? 'Add to' : 'Remove from ' }} contacts list
+       </button>
     </div>
 
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import ButtonCounter from './ButtonCounter.vue';
 
-const message = ref('Hello Component!!!!')
+const props = defineProps({ 
+    name : {type: String, required : true, default : 'Missing name' }, 
+    email : String, 
+    phone : Number, 
+    variable: String,
+    isFavorite: Boolean }) // specify a validation for Props
+
+    function handleClick(name : String){
+        console.log(name)
+    }
 </script>
